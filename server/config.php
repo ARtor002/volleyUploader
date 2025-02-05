a410c6c89bb6a4c963d8ea4115ebf977584e6861
ARtor <armintorkamandi83@gmail.com>
2025-02-06 03:23:07 +0330

    Moved files into 'client' and 'server' folders

---
+++ server/config.php
@@ -0,0 +1,13 @@
+<?php
+// class config {
+//     public static $host = "sql209.infinityfree.com";
+//     public static $dbname = "if0_38143751_volley";
+//     public static $username = "if0_38143751";
+//     public static $password = "XRjzIpH7FAUHuW";
+// }
+class config {
+    public static $host = "127.0.0.1";
+    public static $dbname = "armin";
+    public static $username = "root";
+    public static $password = "";
+}
